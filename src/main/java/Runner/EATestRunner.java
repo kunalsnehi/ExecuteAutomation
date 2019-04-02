e807d8514235f3fa89595fd18b7d182b0e07cc06
kunalsnehi <kunal.snehi07@gmail.com>
2019-04-02 21:48:39 +0530

    Path updated

--- src/main/java/Runner/EATestRunner.java
+++ src/main/java/Runner/EATestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="C:/Users/kunal/workspace/FreeCRMBDDFramework/src/main/java/Features/LoginEA.feature"
+		features="C:/Users/kunal/git/ExecuteAutomation/src/main/java/Features/LoginEA.feature"
 		,glue={"stepDefinations"}
 		//,format={"pretty","html:test-output"}
 		
